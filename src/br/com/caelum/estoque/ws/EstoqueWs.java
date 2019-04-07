e1ece7666b000ddce68a19a914d8a110e1257c2d
mariana barreiros <maridepp1@hotmail.com>
2019-04-06 21:29:29 -0300

    adding a simple service named EstoqueWs that get all itens

---
+++ src/br/com/caelum/estoque/ws/EstoqueWs.java
@@ -0,0 +1,21 @@
+package br.com.caelum.estoque.ws;
+
+import java.util.List;
+
+import javax.jws.WebService;
+
+import br.com.caelum.estoque.modelo.item.*;
+
+//Para dizer que é um serviço web
+@WebService
+public class EstoqueWs {
+	
+	private ItemDao dao = new ItemDao();
+
+	public List<Item> getItens(){
+		
+		System.out.println("Chamando o método getItns()");
+		List<Item> lista = dao.todosItens();
+		return lista;
+	}
+}
